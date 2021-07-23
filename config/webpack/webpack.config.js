1a4bb4311e549fc79581e73887e805f1a411c05a
Lucas de Aguilar <lucasdeaguilarjc@hotmail.com>
2021-07-22 22:15:51 -0300

    [MS-01] Adds initial setup with react, typescript and webpack

---
+++ config/webpack/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.tsx',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../../dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ],
+  devServer: {
+    contentBase: './public',
+    host: '0.0.0.0',
+    port: 4000,
+    compress: true,
+    historyApiFallback: true,
+    hot: true
+  }
+}
