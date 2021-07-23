32afae07114c341d84d1676003727db59e094ecb
Lucas de Aguilar <lucasdeaguilarjc@hotmail.com>
2021-07-23 11:14:41 -0300

    [MS-01] Adds test setup with jest and react testing library

---
+++ config/jest/jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  preset: 'ts-jest',
+  testMatch: ['**/*.spec.{ts,tsx}'],
+  rootDir: '../../',
+  testEnvironment: 'jsdom',
+  verbose: true,
+  collectCoverage: false,
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!**/node_modules/**',
+    '!**/dist/**'
+  ],
+  setupFilesAfterEnv: [
+    '<rootDir>/config/jest/jest-setup.ts'
+  ],
+  coverageDirectory: '<rootDir>/config/jest/coverage',
+}
