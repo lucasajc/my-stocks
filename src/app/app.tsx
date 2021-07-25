f5c3cb23a85271fc1c3eec1d6d2210d0f3b284a0
Lucas de Aguilar <lucasdeaguilarjc@hotmail.com>
2021-07-25 15:09:50 -0300

    [MS-03] Adds production API_TOKEN to circleci

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -7,6 +7,8 @@ import { AppContainer } from './app.styled'
 import './app.css'
 
 const App = () => {
+  console.log(process.env.API_URL)
+  console.log(process.env.API_TOKEN)
   return (
     <Provider store={store}>
       <ThemeProvider theme={main}>
