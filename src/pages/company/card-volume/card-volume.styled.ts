16f3e98cd05fd30e3e7fa0f16fb63b60973fded2
Lucas de Aguilar <lucasdeaguilarjc@hotmail.com>
2021-07-27 18:47:08 -0300

    [MS-03] Changes spacing in the footer of the card volume component

---
+++ src/pages/company/card-volume/card-volume.styled.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+import { CardFooter } from 'components/card/card.component'
+
+export const CardFooterStyled = styled(CardFooter)`
+  > * {
+    margin-right: 12px;
+  }
+`
