e0abbade41c2d67b4cbb5f47e462fe79f54fe09d
Lucas de Aguilar <lucasdeaguilarjc@hotmail.com>
2021-07-27 18:42:55 -0300

    [MS-03] Adds size limit based on viewport to card component

--- src/components/card/card.component.ts
+++ src/components/card/card.component.ts
@@ -4,14 +4,20 @@ export const Card = styled.div`
   background: ${(props) => props.theme.card.backgroundColor};
   padding: 24px;
   border-radius: 16px;
+  box-shadow: 1px 1px 2px #ddd;
 
   @media only screen and (min-width: 480px) {
-    width: 288px;
+    min-width: 266px;
+    flex: 1;
   }
 `
 
 export const CardHeader = styled.div`
   padding: 0 0 8px;
+`
+
+export const CardFooter = styled.div`
+  padding-top: 16px;
 `
 
 export const CardPrimaryContent = styled.span`
