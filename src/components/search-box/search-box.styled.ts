79133ad37dd9730eff29e27314445329a790b174
Lucas de Aguilar <lucasdeaguilarjc@hotmail.com>
2021-07-28 20:51:54 -0300

    [MS-03] Changes search box sizings

--- src/components/search-box/search-box.styled.ts
+++ src/components/search-box/search-box.styled.ts
@@ -4,11 +4,13 @@ import { Button as UIButton } from 'components/button/button.component'
 
 export const Container = styled.form`
   display: flex;
-  margin-bottom: 16px;
+  margin-bottom: 32px;
 `
 
 export const Input = styled(UIInput)`
-  width: 280px;
+  max-width: 280px;
+  min-width: 148px;
+  width: 100%;
   margin-right: 8px;
 `
 
