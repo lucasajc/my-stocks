2fcbec446b2aab995b927e07fd72b7c3c99be21a
Lucas de Aguilar <lucasdeaguilarjc@hotmail.com>
2021-07-28 00:59:14 -0300

    [MS-03] Fixes page scroll

--- src/app/app.styled.ts
+++ src/app/app.styled.ts
@@ -1,9 +1,10 @@
 import styled from 'styled-components'
 
 export const AppContainer = styled.div`
-  position: absolute;
+  position: fixed;
   width: 100%;
   height: 100%;
+  overflow-y: auto;
   font-family: ${(props) => props.theme.text.font};
   background: ${(props) => props.theme.body.backgroundColor};
 `
