3aa7d77a024a9e537dc9368c94beeccab157393f
Lucas de Aguilar <lucasdeaguilarjc@hotmail.com>
2021-07-29 18:23:47 -0300

    [MS-04] Refactors builders

--- src/common/builders/company.builder.ts
+++ src/common/builders/company.builder.ts
@@ -1,48 +1,29 @@
 import { Company } from 'client-api/company'
 
-class CompanyBuilder implements Company {
-  CEO: string
-  address: string
-  address2: string
-  city: string
-  companyName: string
-  country: string
-  description: string
-  employees: number
-  exchange: string
-  industry: string
-  issueType: string
-  phone: string
-  primarySicCode: number
-  sector: string
-  securityName: string
-  state: string
-  symbol: string
-  tags: string[]
-  website: string
-  zip: string
-
+class CompanyBuilder extends Company {
   constructor() {
-    this.symbol = 'some symbol'
-    this.companyName = 'some company name'
-    this.exchange = 'some exchange'
-    this.industry = 'some industry'
-    this.website = 'http://some.website'
-    this.description = 'some description'
-    this.CEO = 'some CEO'
-    this.securityName = 'some security name'
-    this.issueType = 'some issue type'
-    this.sector = 'some sector'
-    this.primarySicCode = 7460
-    this.employees = 350674
-    this.tags = ['some-tag', 'another-tag']
-    this.address = 'some address'
-    this.address2 = 'some address 2'
-    this.state = 'some state'
-    this.city = 'some city'
-    this.zip = '420-102751'
-    this.country = 'US'
-    this.phone = '19869085495'
+    super({
+      symbol: 'some symbol',
+      companyName: 'some company name',
+      exchange: 'some exchange',
+      industry: 'some industry',
+      website: 'http://some.website',
+      description: 'some description',
+      CEO: 'some CEO',
+      securityName: 'some security name',
+      issueType: 'some issue type',
+      sector: 'some sector',
+      primarySicCode: 7460,
+      employees: 350674,
+      tags: ['some-tag', 'another-tag'],
+      address: 'some address',
+      address2: 'some address 2',
+      state: 'some state',
+      city: 'some city',
+      zip: '420-102751',
+      country: 'US',
+      phone: '19869085495',
+    })
   }
 
   withSymbol(symbol: string) {
--- src/common/builders/quote.builder.ts
+++ src/common/builders/quote.builder.ts
@@ -1,118 +1,64 @@
 import { Quote } from 'client-api/quote'
 
-class QuoteBuilder implements Quote {
-  avgTotalVolume: number
-  calculationPrice: 'tops' | 'sip' | 'previousclose' | 'close' | 'iexlasttrade'
-  change: number
-  changePercent: number
-  close: number
-  closeSource: string
-  closeTime: number
-  companyName: string
-  delayedPrice: number
-  delayedPriceTime: number
-  extendedChange: number
-  extendedChangePercent: number
-  extendedPrice: number
-  extendedPriceTime: number
-  high: number
-  highSource: string
-  highTime: number
-  iexAskPrice: number
-  iexAskSize: number
-  iexBidPrice: number
-  iexBidSize: number
-  iexClose: number
-  iexCloseTime: number
-  iexLastUpdated: number
-  iexMarketPercent: number
-  iexOpen: number
-  iexOpenTime: number
-  iexRealtimePrice: number
-  iexRealtimeSize: number
-  iexVolume: number
-  isUSMarketOpen: boolean
-  lastTradeTime: number
-  latestPrice: number
-  latestSource: string
-  latestTime: string
-  latestUpdate: number
-  latestVolume: number
-  low: number
-  lowSource: string
-  lowTime: number
-  marketCap: number
-  oddLotDelayedPrice: number
-  oddLotDelayedPriceTime: number
-  open: number
-  openSource: string
-  openTime: number
-  peRatio: number
-  previousClose: number
-  previousVolume: number
-  primaryExchange: string
-  symbol: string
-  volume: number
-  week52High: number
-  week52Low: number
-  ytdChange: number
-
+class QuoteBuilder extends Quote {
   constructor() {
-    this.symbol = 'some symbol'
-    this.companyName = 'some company name'
-    this.primaryExchange = 'some primary exchange'
-    this.calculationPrice = 'iexlasttrade'
-    this.open = null
-    this.openTime = null
-    this.openSource = 'official'
-    this.close = null
-    this.closeTime = null
-    this.closeSource = 'official'
-    this.high = null
-    this.highTime = 1627329326281
-    this.highSource = '15 minute delayed price'
-    this.low = null
-    this.lowTime = 1627306213268
-    this.lowSource = '15 minute delayed price'
-    this.latestPrice = 142.77
-    this.latestSource = 'IEX Last Trade'
-    this.latestTime = 'July 26 2021'
-    this.latestUpdate = 1627329595647
-    this.latestVolume = null
-    this.iexRealtimePrice = 142.77
-    this.iexRealtimeSize = 44
-    this.iexLastUpdated = 1627329595647
-    this.delayedPrice = null
-    this.delayedPriceTime = null
-    this.oddLotDelayedPrice = null
-    this.oddLotDelayedPriceTime = null
-    this.extendedPrice = null
-    this.extendedChange = null
-    this.extendedChangePercent = null
-    this.extendedPriceTime = null
-    this.previousClose = 141.34
-    this.previousVolume = 4474157
-    this.change = 1.43
-    this.changePercent = 0.01012
-    this.volume = null
-    this.iexMarketPercent = 0.018703847808944834
-    this.iexVolume = 79366
-    this.avgTotalVolume = 5477257
-    this.iexBidPrice = 0
-    this.iexBidSize = 0
-    this.iexAskPrice = 0
-    this.iexAskSize = 0
-    this.iexOpen = 142.75
-    this.iexOpenTime = 1627329588932
-    this.iexClose = 142.77
-    this.iexCloseTime = 1627329595647
-    this.marketCap = 127568254154
-    this.peRatio = 23.87
-    this.week52High = 152.84
-    this.week52Low = 101.89
-    this.ytdChange = 0.16065216386169853
-    this.lastTradeTime = 1627329595647
-    this.isUSMarketOpen = false
+    super({
+      symbol: 'some symbol',
+      companyName: 'some company name',
+      primaryExchange: 'some primary exchange',
+      calculationPrice: 'iexlasttrade',
+      open: null,
+      openTime: null,
+      openSource: 'official',
+      close: null,
+      closeTime: null,
+      closeSource: 'official',
+      high: null,
+      highTime: 1627329326281,
+      highSource: '15 minute delayed price',
+      low: null,
+      lowTime: 1627306213268,
+      lowSource: '15 minute delayed price',
+      latestPrice: 142.77,
+      latestSource: 'IEX Last Trade',
+      latestTime: 'July 26 2021',
+      latestUpdate: 1627329595647,
+      latestVolume: null,
+      iexRealtimePrice: 142.77,
+      iexRealtimeSize: 44,
+      iexLastUpdated: 1627329595647,
+      delayedPrice: null,
+      delayedPriceTime: null,
+      oddLotDelayedPrice: null,
+      oddLotDelayedPriceTime: null,
+      extendedPrice: null,
+      extendedChange: null,
+      extendedChangePercent: null,
+      extendedPriceTime: null,
+      previousClose: 141.34,
+      previousVolume: 4474157,
+      change: 1.43,
+      changePercent: 0.01012,
+      volume: null,
+      iexMarketPercent: 0.018703847808944834,
+      iexVolume: 79366,
+      avgTotalVolume: 5477257,
+      iexBidPrice: 0,
+      iexBidSize: 0,
+      iexAskPrice: 0,
+      iexAskSize: 0,
+      iexOpen: 142.75,
+      iexOpenTime: 1627329588932,
+      iexClose: 142.77,
+      iexCloseTime: 1627329595647,
+      marketCap: 127568254154,
+      peRatio: 23.87,
+      week52High: 152.84,
+      week52Low: 101.89,
+      ytdChange: 0.16065216386169853,
+      lastTradeTime: 1627329595647,
+      isUSMarketOpen: false,
+    })
   }
 
   withSymbol(symbol: string) {
