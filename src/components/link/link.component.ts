f61f9ca3f459b2b22174829fa44c7dd690ff6630
Lucas de Aguilar <lucasdeaguilarjc@hotmail.com>
2021-08-02 13:09:57 -0300

    Removes text-decoration from link component

--- src/components/link/link.component.ts
+++ src/components/link/link.component.ts
@@ -3,6 +3,7 @@ import { Link as LinkUI } from 'react-router-dom'
 
 export const Link = styled(LinkUI)`
   transition: color 0.1s ease-in;
+  text-decoration: none;
   color: ${(props) => props.theme.link.default.color};
   &:hover {
     color: ${(props) => props.theme.link.hover.color};
